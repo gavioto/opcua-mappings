557d8a7bc990d28b705d0e24faae88d119f4b198
Alexander Rykovanov <rykovanov.as@gmail.com>
2014-01-09 15:54:28 +0400

    Debian package version takes from configure.ac
    Debian packaging.

--- make_deb.sh
+++ make_deb.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-make dist
-tar -xvf opcuamappings-0.1.2.tar.gz
-cd opcuamappings-0.1.2
-dh_make -y -l -c lgpl3 -e rykovanov.as@gmail.com -f ../opcuamappings-0.1.2.tar.gz
-#cp debian -rvf opcuamappings-0.1.2
-#dpkg-source --commit
-dpkg-buildpackage -rfakeroot
+tar -xvf opcuamappings-${VERSION}.tar.gz
+cd opcuamappings-${VERSION}
+dh_make -y -l -c lgpl3 -e rykovanov.as@gmail.com -f ../opcuamappings-${VERSION}.tar.gz
+dpkg-buildpackage -rfakeroot -nc -F
 
