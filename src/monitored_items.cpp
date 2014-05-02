d6957b474b310ec1c8d972c7fc4287968f35c549
Olivier R-D <olivier@dummy.com>
2014-05-02 20:11:04 +0200

    add some missing serialize methods to support monitoredItems

--- src/monitored_items.cpp
+++ src/monitored_items.cpp
@@ -67,6 +67,30 @@ namespace OpcUa
       *this << result.FilterResult;
     }
 
+   template<>
+    void DataDeserializer::Deserialize<CreateMonitoredItemsResult>(CreateMonitoredItemsResult& params)
+    {
+      *this >> params.Status;
+      *this >> params.MonitoredItemID;
+      *this >> params.RevisedSamplingInterval;
+      *this >> params.RevizedQueueSize;
+      *this >> params.FilterResult;
+    }
+
+    template<>
+    void DataSerializer::Serialize<std::vector<CreateMonitoredItemsResult>>(const std::vector<CreateMonitoredItemsResult>& targets)
+    {
+      SerializeContainer(*this, targets);
+    }
+
+    template<>
+    void DataDeserializer::Deserialize<std::vector<CreateMonitoredItemsResult>>(std::vector<CreateMonitoredItemsResult>& targets)
+    {
+      DeserializeContainer(*this, targets);
+    }
+
+
+
     ////////////////////////////////////////////////////////////////
 
     template <>
@@ -81,6 +105,29 @@ namespace OpcUa
       SerializeContainer(*this, data.Results, 0);
       SerializeContainer(*this, data.Diagnostics, 0);
     }
+
+    template<>
+    void DataDeserializer::Deserialize<MonitoredItemsData>(MonitoredItemsData& params)
+    {
+      *this >> params.Results;
+      *this >> params.Diagnostics;
+    }
+    
+    template<>
+    void DataSerializer::Serialize<std::vector<MonitoredItemsData>>(const std::vector<MonitoredItemsData>& targets)
+    {
+      SerializeContainer(*this, targets);
+    }
+
+    template<>
+    void DataDeserializer::Deserialize<std::vector<MonitoredItemsData>>(std::vector<MonitoredItemsData>& targets)
+    {
+      DeserializeContainer(*this, targets);
+    }
+
+
+
+
 
     ////////////////////////////////////////////////////////////////
 
@@ -97,6 +144,16 @@ namespace OpcUa
       *this << response.Header;
       *this << response.Data;
     }
+
+    template<>
+    void DataDeserializer::Deserialize<CreateMonitoredItemsResponse>(CreateMonitoredItemsResponse& params)
+    {
+      *this >> params.TypeID;
+      *this >> params.Header;
+      *this >> params.Data;
+    }
+
+
 
   }
 }
